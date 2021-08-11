3734a69c7ec3520fd4614e2a7074a8491db4a684
inayaZahid <82757716+inayaZahid@users.noreply.github.com>
2021-08-11 15:14:18 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,89 @@
+var gkImg;
+var question;
+function preload(){
+gkImg=loadImage("quizziz.png");
+}
+function setup(){
+  createCanvas(displayWidth,displayHeight)
+math=createButton('Math')
+math.position(displayWidth/2-250,displayHeight/2-150);
+math.size(150,50)
+
+math.mouseClicked(mathQuiz);
+Gk=createButton('Gk')
+Gk.position(displayWidth/2+250,displayHeight/2-150);
+Gk.size(150,50)
+Gk.mouseClicked(gkQuiz);
+
+}
+function draw(){
+
+
+drawSprites();
+}
+function mathQuiz(){
+easy1=createButton('Easy')
+easy1.position(displayWidth/2,displayHeight/2-100);
+easy1.size(120,50);
+medium1=createButton('Medium')
+medium1.position(displayWidth/2,displayHeight/2-50);
+medium1.size(120,50)
+hard1=createButton('Hard');
+hard1.position(displayWidth/2,displayHeight/2);
+hard1.size(120,50);
+
+}
+function gkQuiz(){
+ background(gkImg);
+  easy=createButton('Easy')
+easy.position(displayWidth/2,displayHeight/2-100);
+easy.size(120,50);
+easy.mouseClicked(gkEasy);
+medium=createButton('Medium')
+medium.position(displayWidth/2,displayHeight/2-50);
+medium.size(120,50)
+hard=createButton('Hard');
+hard.position(displayWidth/2,displayHeight/2);
+hard.size(120,50);
+}
+function gkEasy(){
+ 
+  question=createButton( "Q1. The natural setellite of the Earth is-?" )
+question.position(displayWidth/4,displayHeight/4)
+math.size(0,0)
+gk.size(0,0);
+asy.size(0,0);
+medium.size(0,0);
+hard.size(0,0);
+ option1=createButton("A:32")
+
+  option1.position(displayWidth/4,displayHeight/4+40);
+  option2  =createButton("B:34")
+option2.position(displayWidth/4+50,displayHeight/4+40);
+option3=createButton("C:28");
+option3.position(displayWidth/4+100,displayHeight/4+40);
+  question=createButton("Q2. How many teeth does an adult have?")
+
+question=createButton("Q3. The capital of United States Of America is?")
+  question=createButton("the smallest country in the world is?")
+question=createButton("how much percentage of Nitrogen consist in Earth's atmosphere?")
+}
+
+/*function gkMedium(){
+  question.html("National Flower of India is?")
+  this.question.html("Which of thes is not a lumnious object?")
+  this.question.html("how many planets are there in our Solar System now?")
+  this.question.html("which is the largest Country in the world?");
+  this.question.html("which country's flag doesn't have red, white, or blue color?")
+ 
+}
+function gkHard(){
+  this.question.html("which of these is not a vegetable?");
+  this.question.html("which is the biggest city in the world?");
+  this.question.html("the most expensive Toy company in world is?")
+  this.question.html("Where is Yellow River?");
+  this.question.html("which of these is not a Space Station?");
+}*/
+function hide(){
+  gkQuiz.hide();
+}
